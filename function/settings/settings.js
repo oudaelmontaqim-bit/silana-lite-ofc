c2e8878999ecd1d1d7ffa4e6db512fcdf1022787
oudaelmontaqim-bit <oudaelmontaqim@gmail.com>
2026-06-20 08:51:58 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -15,7 +15,7 @@ global.setting = {
 
 global.info = {
  nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ pairingNumber : '212728089990',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
